30d42fd5d1609790cc58b860b420d0e274b8eb9b
ivan <parfenov_ivan_42a@mail.ru>
2024-01-29 11:38:06 +0700

    init

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
+use teloxide::Bot;
+use teloxide::repls::CommandReplExt;
+use crate::r#mod::tg_bot::{answer, Command};
+
 mod r#mod;
 
-fn main() {
-
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+    log::info!("Starting command bot...");
+    let bot = Bot::from_env();
+    Command::repl(bot, answer).await;
     // Infinite loop
     loop {
-        println!("oops");
+
     }
 }
